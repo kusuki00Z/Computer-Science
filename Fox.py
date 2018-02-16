207e6e274bee002fb1209a46bd2bde957f6f0dc9
kusuki00Z <36524184+kusuki00Z@users.noreply.github.com>
2018-02-15 17:04:32 -0800

    Add files via upload

---
+++ Fox.py
@@ -0,0 +1,40 @@
+# -----------------------------------------+
+# Shengnan Zhou, Alex Tseng                |
+# CSCI 107, Assignment 4                   |
+# Last Updated: February 16, 2018          |
+# -----------------------------------------|
+# Use loops to minimize repeated words and |
+# phrases to these lyrics of "The Fox".    |
+# -----------------------------------------+
+
+
+
+
+
+print("Dog goes woof, cat goes meow.")
+print("Bird goes tweet, and mouse goes squeak.")
+print("Cow goes moo. Frog goes croak, and the elephant goes toot.")
+
+
+print("Ducks say quack and fish go blub, and the seal goes"+" ow"*3+".")
+
+
+
+print("But there's one sound that no one knows...")
+print("WHAT DOES THE FOX SAY?")
+
+
+
+print()
+
+
+
+print("Ring"+" ding"*3+" dingeringeding!")
+
+for i in range(2):
+    print("Gering"+" ding"*3+" dingeringeding!")
+
+print("WHAT DOES THE FOX SAY?")
+
+for i in range(3):
+    print("Wa"+" pa"*5+" pow!")
